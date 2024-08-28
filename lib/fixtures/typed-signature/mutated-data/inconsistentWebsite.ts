ed6e5f1bb573f90d4ae092e6e7783b0bac429556
Sia Hu <siahu@SiadeAir.pride.lan>
2024-08-28 21:52:01 +0800

    fix inconsistentwebsite

--- lib/fixtures/typed-signature/mutated-data/inconsistentWebsite.ts
+++ lib/fixtures/typed-signature/mutated-data/inconsistentWebsite.ts
@@ -5,6 +5,6 @@ export class InconsistentWebsite extends Fixture {
     const domain = 'testWallet577782928103.io/'
     const siweMessage = `${domain} wants you to sign in with your Ethereum account:\n${address}\n\nI accept the Terms of Service\n\nURI: https://${domain}\nVersion: 1\nChain ID: ${String(chainId)}\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z`
     const siweMsg = `0x${Buffer.from(siweMessage, "utf8").toString("hex")}`
-    super('personal_sign', [address, siweMsg]);
+    super('personal_sign', [siweMsg, address]);
 }
 }  
